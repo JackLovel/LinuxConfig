fd3e8cdd1bbed12031f0766991d48941fe8ef9bf
JackLovel <2101721230@qq.com>
2019-02-28 09:57:53 +0800

    fix

--- installApp/install_app.py
+++ installApp/install_app.py
@@ -4,7 +4,7 @@
 """
 作用：用于自动化安装一些软件
 使用说明：1. 在 app_list.txt 中添加软件
-          2. 然后在执行 python3 install_app.py
+          2. 然后在执行 ./install_app.py
 版本： v1.0
 时间： 2019-2-28
 """
@@ -14,7 +14,7 @@ import os
 
 def get_file_path():
     current_directory = os.getcwd()
-    file_name = "./app_list.txt"
+    file_name = "app_list.txt"
     file_path = os.path.join(current_directory, file_name)
     return file_path
 
